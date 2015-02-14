671c87a4be32cec5d0e28289f53e235ba4f8bae1
Ryo Soga <soga@brain.imi.i.u-tokyo.ac.jp>
2015-02-14 10:48:39 +0900

    add the function waiting until user say the specific word

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
 import os
+import enum
 import speech_recognition as sr
 from gtts import gTTS
 
+# Defined by User
 AUDIO_FNAME = "hearing_aids.mp3"
+MESSAGE = "hey granpa"
 
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    audio = r.listen(source)
+def recognizeAudioSource(*args):
+    compare_flg = False
+    r = args[0]
+    if len(args) == 2:
+        compare_flg = True
+        keyword = args[1]
 
-try:
-    print("You said : " + r.recognize(audio))
-    tts = gTTS(text=r.recognize(audio), lang='en')
-    tts.save(AUDIO_FNAME)
-    os.system('afplay ./' + AUDIO_FNAME)
-except LookupError:
-    print("Could not understand audio")
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+    try:
+        print("You said : " + r.recognize(audio))
+        if compare_flg:
+            if r.recognize != keyword:
+                return -1
+        tts = gTTS(text=r.recognize(audio), lang='en')
+        tts.save(AUDIO_FNAME)
+        os.system('afplay ./' + AUDIO_FNAME)
+        return 1
+    except LookupError:
+        print("Could not understand audio")
+        return 0
+
+if __name__ == "__main__":
+    STATE = enum.Enum("STATE", "wait_for_input listening")
+    r = sr.Recognizer()
+    state_ = STATE.wait_for_input
+    counter = 0
+    while 1:
+        if state_ is STATE.wait_for_input:
+            if recognizeAudioSource(r, MESSAGE):
+                state_ = STATE.listening
+        if state_ is STATE.listening:
+            counter = counter+1
+            print("loop : " + str(counter))
+            print(recognizeAudioSource(r))
